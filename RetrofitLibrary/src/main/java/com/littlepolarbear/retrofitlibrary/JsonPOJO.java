8b8a46c6bf88850260af49c468f360e68665fcf3
Little Polar Bear <34699290+Little-Polar-Bear@users.noreply.github.com>
2021-09-29 14:08:56 +1300

    Update JsonPOJO.java

--- RetrofitLibrary/src/main/java/com/littlepolarbear/retrofitlibrary/JsonPOJO.java
+++ RetrofitLibrary/src/main/java/com/littlepolarbear/retrofitlibrary/JsonPOJO.java
@@ -1,4 +1,4 @@
-package com.littlepolarbear.retrofitlibrary;
+package com.littlepolarbear.retrofitlibrary
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
