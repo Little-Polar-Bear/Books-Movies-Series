639a8fd897b014b6f28de91d103242e292f1a462
Little Polar Bear <34699290+Little-Polar-Bear@users.noreply.github.com>
2021-09-29 21:39:06 +1300

    Update DetailActivity.java

--- app/src/main/java/com/littlepolarbear/tvmoviereviews/activities/DetailActivity.java
+++ app/src/main/java/com/littlepolarbear/tvmoviereviews/activities/DetailActivity.java
@@ -1,4 +1,4 @@
-package com.littlepolarbear.tvmoviereviews.activities
+package com.littlepolarbear.tvmoviereviews.activities;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
